69b3e82926c518d32897bfe309ae56d85917a4a6
김초희 <qwerpoiu1316@naver.com>
2020-09-18 11:38:12 +0900

    Add Codes : api 호출 테스트 코드 작성

--- chrome-extension/src/App.js
+++ chrome-extension/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import Login from './containers/Login';
+import { Route, Switch, Router } from 'react-router-dom';
+import Item from './containers/Item';
 
 function App() {
   return (
     <div className="App">
+      {/* <Switch>
+        <Router>
+        <Route path="/" component={Login} exact />
+        <Route path="/item" component={Item} exact />
+        </Router>
+        
+      </Switch> */}
       <Login></Login>
+      {/* <Router>
+        <Route path="/item" component={Item} exact />
+      </Router> */}
     </div>
   );
 }
---
+++ chrome-extension/src/containers/Item.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Item() {
+    return(
+        <>
+            <div>하이하이</div>
+        </>
+    )
+}
+
+export default Item;
